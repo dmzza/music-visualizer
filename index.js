acc9ed726e5f141d0763319cb8753eeed105f477
David Mazza <davidedwardmazza@gmail.com>
2021-02-11 19:31:30 -0800

    begin implementing audio analyser
    just to see if I can get any data from it. So far no luck 
    in any browser

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 let canvas
 /** @type CanvasRenderingContext2D */
 let context
+/** @type AudioElement */
+let audioElement
+/** @type AudioContext */
+let audioContext = 
+  // new AudioContext()
+  new webkitAudioContext()
+/** @type AnalyserNode */
+let analyser = audioContext.createAnalyser()
+analyser.fftSize = 2048 // max: 2048
+/** @type MediaElementAudioSourceNode */
+let source
+/** @type Uint8Array */
+let data
 let i = 0
 
 window.addEventListener('DOMContentLoaded', function() {
   canvas = document.getElementById("visualization")
   context = canvas.getContext("2d")
+  audioElement = document.getElementById("source")
+  audioElement.addEventListener("play", function () {
+    source = audioContext.createMediaElementSource(audioElement)
+    source.connect(analyser)
+    source.connect(audioContext.destination)
+    data = new Uint8Array(analyser.frequencyBinCount);
+  })
 })
 
 function draw() {
@@ -13,6 +33,9 @@ function draw() {
   if(i > 1000) {
     return;
   }
+  analyser.getByteFrequencyData(data)
+  let normalData = [...data]
+  console.log(normalData)
   context.beginPath()
   context.moveTo(100, 200)
   context.lineTo(200, 200)
