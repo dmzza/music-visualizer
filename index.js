03b562864c6050444531ce1366e0eb37e2ceab93
David Mazza <davidedwardmazza@gmail.com>
2021-02-11 17:21:19 -0800

    create a simple draw() loop

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-alert("hello")
+let canvas
+/** @type CanvasRenderingContext2D */
+let context
+let i = 0
+
+function didLoad() {
+  canvas = document.getElementById("visualization")
+  context = canvas.getContext("2d")
+}
+
+function draw() {
+  i++;
+  if(i > 1000) {
+    return;
+  }
+  context.beginPath()
+  context.moveTo(100, 200)
+  context.lineTo(200, 200)
+  context.lineTo(200, 100)
+  context.lineTo(100, 200)
+  context.closePath()
+
+  context.stroke()
+  context.rotate(-0.1)
+  setTimeout(draw, 10)
+}
