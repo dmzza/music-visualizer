f903f10912989d18a40290a9caeb996c0dd93596
David Mazza <davidedwardmazza@gmail.com>
2021-02-13 14:58:53 -0800

    implement exponential smoothing
    and refactor two dimensional moving average 
    so that it is just another function I can use

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ window.addEventListener('DOMContentLoaded', function() {
     source.connect(audioContext.destination)
     data = new Uint8Array(analyser.frequencyBinCount);
     lastData = new Array(analyser.frequencyBinCount);
+    lastData.fill(128)
   })
 })
 
@@ -47,14 +48,16 @@ function draw(delay) {
   // analyser.getByteFrequencyData(data)
   analyser.getByteTimeDomainData(data)
   let normalData = [...data]
+  let smoothData = exponentialSmoothing(0.3, normalData, lastData)
+  // let smoothData = twoDimensionalMovingAverage(normalData, lastData)
   context.clearRect(0, 0, canvas.width, canvas.height);
-  let space = canvas.width / data.length;
-  normalData.forEach((value, j)=>{
+  const dataLength = data.length
+  const space = canvas.width / dataLength
+  const maxIndex = dataLength - 1
+  smoothData.forEach((value, j)=>{
     let color
-    let previousIndex = Math.max(j-1,0)
-    let nextIndex = Math.min(j+1, analyser.frequencyBinCount)
-    let averageValue = (lastData[previousIndex] + lastData[j] + lastData[nextIndex] +
-      normalData[previousIndex] + normalData[j] + normalData[nextIndex]) / 6
+    let previousIndex = Math.max(j-1, 0)
+    let nextIndex = Math.min(j+1, maxIndex)
     if(value < 128) {
       if(delay && lastData[j] < 128 && lastData[previousIndex] < 128 && lastData[nextIndex] < 128) {
         color = blue
@@ -72,10 +75,28 @@ function draw(delay) {
     context.strokeStyle = color
     context.lineWidth = 1
     context.moveTo(space * j, canvas.height);
-    context.lineTo(space * j, canvas.height-averageValue);
+    context.lineTo(space * j, canvas.height-value);
     // context.lineTo(space * j, canvas.height-128);
     context.stroke();
   })
-  lastData = normalData
+  lastData = smoothData
   requestAnimationFrame(() => draw(true))
+}
+
+function exponentialSmoothing(alpha, data, previousSmoothData) {
+  return data.map((value, i) => {
+    return alpha * value + (1 - alpha) * previousSmoothData[i]
+  })
+}
+
+/* averages data over adjacent data points and across time */
+function twoDimensionalMovingAverage(data, previousData) {
+  let maxIndex = data.length - 1
+  
+  return data.map((value, i) => {
+    let previousIndex = Math.max(i-1, 0)
+    let nextIndex = Math.min(i+1, maxIndex)
+    return (previousData[previousIndex] + previousData[i] + previousData[nextIndex] +
+            data[previousIndex] + data[i] + data[nextIndex]) / 6
+  })
 }
