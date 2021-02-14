a87f6f1a688aa668c8173d1a9e3520103d270788
David Mazza <davidedwardmazza@gmail.com>
2021-02-14 00:26:07 -0800

    implement a low pass filter using the Web Audio API example

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ let audioContext = constructAudioContext()
 /** @type AnalyserNode */
 let analyser = audioContext.createAnalyser()
 analyser.fftSize = 2048 // max: 2048
+let isFiltered = false
+/** @type IIRFilterNode */
+let iirfilter
 /** @type MediaElementAudioSourceNode */
 let source
 /** @type Uint8Array */
@@ -28,8 +31,39 @@ window.addEventListener('DOMContentLoaded', function() {
   audioElement = document.getElementById("source")
   audioElement.addEventListener("play", function () {
     source = audioContext.createMediaElementSource(audioElement)
-    source.connect(analyser)
-    source.connect(audioContext.destination)
+    source.connect(analyser).connect(audioContext.destination)
+
+    // Taken from: https://codepen.io/Rumyra/pen/oPxvYB/
+    // change this to change the filter - can be 0-3 and will reference the values in the array below
+    const filterNumber = 0
+
+    let lowPassCoefs = [
+      {
+        frequency: 200,
+        feedforward: [0.00020298, 0.0004059599, 0.00020298],
+        feedback: [1.0126964558, -1.9991880801, 0.9873035442]
+      },
+      {
+        frequency: 500,
+        feedforward: [0.0012681742, 0.0025363483, 0.0012681742],
+        feedback: [1.0317185917, -1.9949273033, 0.9682814083]
+      },
+      {
+        frequency: 1000,
+        feedforward: [0.0050662636, 0.0101325272, 0.0050662636],
+        feedback: [1.0632762845, -1.9797349456, 0.9367237155]
+      },
+      {
+        frequency: 5000,
+        feedforward: [0.1215955842, 0.2431911684, 0.1215955842],
+        feedback: [1.2912769759, -1.5136176632, 0.7087230241]
+      }
+    ]
+
+    let feedForward = lowPassCoefs[filterNumber].feedforward,
+      feedBack = lowPassCoefs[filterNumber].feedback
+    iirfilter = audioContext.createIIRFilter(feedForward, feedBack)
+
     data = new Uint8Array(analyser.frequencyBinCount);
     lastData = new Array(analyser.frequencyBinCount);
     lastData.fill(128)
@@ -128,4 +162,16 @@ function identifyLongCrest(data) {
     i++
   }
   return [startingPoint, endingPoint]
+}
+
+function toggleFilter() {
+  if(isFiltered) {
+    source.disconnect(iirfilter)
+    source.connect(analyser).connect(audioContext.destination)
+    isFiltered = false
+  } else {
+    source.disconnect(analyser)
+    source.connect(iirfilter).connect(analyser).connect(audioContext.destination)
+    isFiltered = true
+  }
 }
